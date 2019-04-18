f705c1b823bdba87fdcf71e959c8fc8a0d28528d
gunjan <gunjankaphle2016@gmail.com>
2019-04-17 20:53:42 -0500

    Add entrys from db

--- jobs/app.py
+++ jobs/app.py
@@ -35,5 +35,6 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name from job join employer on employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
 
